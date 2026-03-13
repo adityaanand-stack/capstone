48c818cf647d52b8ef4b1da19ea691cb5e27b8bd
Aditya Anand <adityaanand@adobe.com>
2026-03-13 15:23:36 +0530

    Update advanturecard.js

--- blocks/advanturecard/advanturecard.js
+++ blocks/advanturecard/advanturecard.js
@@ -1,9 +1,7 @@
 export default function decorate(block) {
-
   const row = block.querySelector(':scope > div');
   const image = row.children[0];
   const content = row.children[1];
-
   image.classList.add('advanture-image');
   content.classList.add('advanture-content');
 
